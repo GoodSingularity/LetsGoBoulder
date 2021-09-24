bceaab956b8f5fb73cb01e66338c7af4233cb273
GoodSingularity <ZMosciszko@protonmail.com>
2021-09-24 19:19:33 +0200

    validations add

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,5 @@
 class Route < ApplicationRecord
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
+    validates :color, presence: true, numericality: { only_integer: true }
+    validates :route_setter, presence: true
 end
