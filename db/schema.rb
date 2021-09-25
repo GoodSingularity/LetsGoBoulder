b59527af3c3acfae16e1daa22af5f85d459e8685
Kamil Mościszko <23016118+GoodSingularity@users.noreply.github.com>
2021-09-25 18:31:09 -0400

    [LSGB-008] Add files to Routes (#8)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_170112) do
+ActiveRecord::Schema.define(version: 2021_09_25_102020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_170112) do
     t.string "route_setter"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "files", default: [], array: true
   end
 
 end
