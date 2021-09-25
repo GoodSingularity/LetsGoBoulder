9e94a6944fb6d9bacf802f64ca0f208fd8d47775
GoodSingularity <ZMosciszko@protonmail.com>
2021-09-25 17:57:26 -0400

    add deleting empty files in minio

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -8,4 +8,10 @@ Aws.config.update(
   region: "us-east-1"
 )
 
-@s3 = Aws::S3::Client.new
+$s3 = Aws::S3::Client.new
+to_remove = []
+$s3.list_objects(bucket: "routes").contents.each do |file|
+  if file.size == 0
+    $s3.delete_object(key: file.key, bucket: "routes")
+  end
+end
