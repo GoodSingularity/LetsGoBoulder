e652398a5428a269b9fb95240c0bdd24af3ff703
GoodSingularity <ZMosciszko@protonmail.com>
2021-09-25 18:30:39 -0400

    controller re-work

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,18 +1,21 @@
+require "securerandom"
 class RoutesController < ApplicationController
   def index
+    name = SecureRandom.uuid
+    color = params[:color]
+    route_setter = params[:route_setter]
     file = params[:file]
     config = {
-      key: "kamilroutess.txt",
+      key: SecureRandom.uuid,
       bucket: "routes"
     }
     $s3.put_object(
       key: config[:key],
       body: file.tempfile.read,
-      bucket: config[:bucket],
-      content_type: "text/plain"
+      bucket: config[:bucket]
     )
-    object = $s3.get_object(bucket: config[:bucket], key: config[:key])
-    object.body.read
-    render json: "succesful".to_json
+    object = $s3.get_object(bucket: config[:bucket], key: config[:key]).body.read
+    route = Route.create(name: name, color: color, route_setter: route_setter, files: [config[:key]])
+    render json: {message: "Boulder has been added", object: route.to_json}
   end
 end
