d1745420f2dba265a39294b4340e3274a49806e8
GoodSingularity <ZMosciszko@protonmail.com>
2021-09-26 07:31:40 -0400

    adding create_route_mutation

--- app/graphql/mutations/create_route_mutation.rb
+++ app/graphql/mutations/create_route_mutation.rb
@@ -4,7 +4,7 @@ module Mutations
       argument :route_setter, String, required: true
 
       def resolve(**args)
-        route = Route.create(name: SecureRandom.uuid, color: color, route_setter: route_setter, files: [])
+        route = Route.create(name: SecureRandom.uuid, color: args[:color], route_setter: args[:route_setter], files: [])
         {status: 200}
       end
     end
