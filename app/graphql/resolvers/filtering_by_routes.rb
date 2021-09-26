331e832d94d792e9e33a2decc2d9e29c7a87d31d
GoodSingularity <ZMosciszko@protonmail.com>
2021-09-26 15:33:59 -0400

    filtering by routes

---
+++ app/graphql/resolvers/filtering_by_routes.rb
@@ -0,0 +1,35 @@
+module Resolvers
+  class FilteringByRoutes < GraphQL::Schema::Resolver
+    description "filtering by routes"
+
+    type [Types::RouteType], null: false
+    argument :color, Int, required: true
+    argument :route_setter, String, required: true
+
+    def resolve(**args)
+      color ||= args[:color]
+      route_setter ||= args[:route_setter]
+      routes = Route.all
+  
+      filtering_routes(routes: routes, color: color, route_setter: route_setter)
+    rescue ActiveRecord::RecordNotFound => error
+      raise GraphQL::ExecutionError, error.message
+    end
+
+    private
+
+    def filtering_routes(routes:, color:, route_setter:)
+      if color.nil?
+        color = -1
+      end
+      if !color.nil?
+        routes = Route.all.where(color: color).filter_by_color
+      end
+  
+      if !route_setter.nil?
+        routes = routes.where(route_setter: route_setter).filter_by_route_setter
+      end
+      routes
+    end
+  end
+end
