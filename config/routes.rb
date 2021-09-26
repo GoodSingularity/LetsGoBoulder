fc57113460befa8e0f1607c7eb04594c37ff3322
Kamil Mościszko <23016118+GoodSingularity@users.noreply.github.com>
2021-09-26 07:21:27 -0400

    [LSGB-016] [Refactoring]Delete search endpoint from Route controller (#15)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   post "/index", to: "routes#add"
   get "/index", to: "routes#index"
-  get "/search", to: "routes#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
