0dc5b797212a0cac00fb3bc330349f8d3c8eb9f1
Kamil Mościszko <23016118+GoodSingularity@users.noreply.github.com>
2021-09-26 15:59:57 -0400

    [LSGB-023] Delete Route Rails Controller (#22)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
-  post "/index", to: "routes#add"
-  get "/index", to: "routes#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
