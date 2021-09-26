b9a426b7624b9d2c889f5873c55c33da70ec291b
GoodSingularity <ZMosciszko@protonmail.com>
2021-09-25 21:54:48 -0400

    add endpoint to search by level, route_setter and name

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -17,6 +17,15 @@ class RoutesController < ApplicationController
     render json: routes
   end
 
+  def search
+    search = eval(params[:filtering])[:search]
+    if %w(black blue green orange yellow white).include? search.downcase
+      search = return_color_id(search)
+    end
+    route = Route.search(search)
+    render json: route
+  end
+
   def add
     name = SecureRandom.uuid
     color = params[:color]
@@ -35,4 +44,20 @@ class RoutesController < ApplicationController
     route = Route.create(name: name, color: color, route_setter: route_setter, files: [config[:key]])
     render json: {message: "Boulder has been added", object: route.to_json}
   end
+
+  private
+
+  def return_color_id(name)
+    levels = Hash[
+      "black": 0, 
+      "blue": 1, 
+      "green": 2, 
+      "orange": 3, 
+      "yellow": 4, 
+      "white": 5
+    ].with_indifferent_access
+
+    levels[name]
+  end
+
 end
