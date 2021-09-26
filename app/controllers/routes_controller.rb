e3c98d6a9a7fb9bc677ae4c5d38f9398380a0b94
GoodSingularity <ZMosciszko@protonmail.com>
2021-09-25 22:10:25 -0400

    final commit

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,25 +1,24 @@
 require "securerandom"
 class RoutesController < ApplicationController
-
   def index
     params[:filtering] = eval(params[:filtering])
-    color ||= params[:filtering][:color] 
+    color ||= params[:filtering][:color]
     route_setter ||= params[:filtering][:route_setter]
     routes = Route.all
 
-    if(color != nil)
+    if !color.nil?
       routes = Route.all.where(color: color)
     end
 
-    if(route_setter != nil)
-        routes = routes.where(route_setter: route_setter)
+    if !route_setter.nil?
+      routes = routes.where(route_setter: route_setter)
     end
     render json: routes
   end
 
   def search
-    search = eval(params[:filtering])[:search]
-    if %w(black blue green orange yellow white).include? search.downcase
+    search = params[:filtering]["search"]
+    if %w[black blue green orange yellow white].include? search.downcase
       search = return_color_id(search)
     end
     route = Route.search(search)
@@ -48,16 +47,13 @@ class RoutesController < ApplicationController
   private
 
   def return_color_id(name)
-    levels = Hash[
-      "black": 0, 
-      "blue": 1, 
-      "green": 2, 
-      "orange": 3, 
-      "yellow": 4, 
-      "white": 5
-    ].with_indifferent_access
+    levels = {black: 0,
+              blue: 1,
+              green: 2,
+              orange: 3,
+              yellow: 4,
+              white: 5}.with_indifferent_access
 
     levels[name]
   end
-
 end
--- spec/requests/routes_spec.rb
+++ spec/requests/routes_spec.rb
@@ -1,7 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "Routes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context "when passes sucessful" do
+    it "search ok" do
+      route = Route.create(name: Faker::Name.name, color: 1, route_setter: "Kacper")
+      get "/search", params: {filtering: {search: "blue"}}
+      expect(response.status).to eq(200)
+    end
+  end
+
+  context "when does not pass sucessfully" do
+    it "search bad" do
+      route = Route.create(name: Faker::Name.name, color: 1, route_setter: "Kacper")
+      get "/search", params: {filtering: {search: "Andrzej"}}
+      expect(eval(response.body).size).to eq(0)
+    end
   end
 end
