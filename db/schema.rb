3f0dd6e52394257039643564427f05489e1a1f8a
Kamil Mościszko <23016118+GoodSingularity@users.noreply.github.com>
2021-10-01 04:07:04 -0400

    [LSGB-029] Adding Model Ascend (#30)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_200721) do
+ActiveRecord::Schema.define(version: 2021_10_01_170112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ascends", force: :cascade do |t|
+    t.uuid "route_id"
+    t.uuid "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.uuid "likes", default: [], array: true
+  end
 
   create_table "routes", force: :cascade do |t|
     t.string "name"
