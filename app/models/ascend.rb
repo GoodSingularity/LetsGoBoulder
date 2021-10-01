97ed51bc3a8ae0d783552552d8ea3723266f7388
GoodSingularity <ZMosciszko@protonmail.com>
2021-10-01 04:06:31 -0400

    add validations to model

---
+++ app/models/ascend.rb
@@ -0,0 +1,7 @@
+class Ascend < ApplicationRecord
+    validates :route_id, :format => {:with => /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/}
+    validates :user_id, :format => {:with => /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/}
+    validates :likes, :format => {:with => /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/}
+
+  end
+  
