9720690af094663f97c1bc83f6b4105c97df048f
GoodSingularity <ZMosciszko@protonmail.com>
2021-10-01 13:51:12 -0400

    searching in users

---
+++ app/graphql/resolvers/search_in_users.rb
@@ -0,0 +1,15 @@
+module Resolvers
+  class SearchInUsers < GraphQL::Schema::Resolver
+    description "Seach in users"
+
+    type [Types::UserType], null: false
+    argument :search, String, required: true
+
+    def resolve(**args)
+      search = args[:search]
+      context[:current_user].nil? ? (raise GraphQL::ExecutionError, "Authentication Error") : User.search(search)
+    rescue ActiveRecord::RecordNotFound => error
+      raise GraphQL::ExecutionError, error.message
+    end
+  end
+end
---
+++ spec/graphql/resolvers/routes/search_in_users_spec.rb
@@ -0,0 +1,55 @@
+require "rails_helper"
+
+module Resolvers
+  module Routes
+    RSpec.describe SearchInRoutes, type: :request do
+      let(:user) {
+        User.create!(
+          name: "test",
+          email: "test@test.com",
+          password: "test"
+        )
+      }
+      let(:context) {
+        ctx = {
+          current_user: user
+        }
+      }
+
+      describe ".resolve not found" do
+        it "search in users" do
+          result = FBoulderSchema.execute(query, variables: {search: ""}, context: context)
+          size = result["data"]["searchInUsers"].size
+          expect(size).to eq(0)
+        end
+      end
+
+      describe ".resolve found" do
+        it "search in users through name" do
+          result = FBoulderSchema.execute(query, variables: {search: user.name}, context: context)
+          size = result["data"]["searchInUsers"].size
+          expect(size).to_not eq(0)
+          expect(result["data"]["searchInUsers"][0]["name"]).to eq(user.name)
+        end
+
+        it "search in users through email" do
+          result = FBoulderSchema.execute(query, variables: {search: user.email}, context: context)
+          size = result["data"]["searchInUsers"].size
+          expect(size).to_not eq(0)
+          expect(result["data"]["searchInUsers"][0]["email"]).to eq(user.email)
+        end
+      end
+
+      def query
+        <<~GQL
+          query($search: String!){
+            searchInUsers(search: $search){
+              name
+              email
+            }
+          }
+        GQL
+      end
+    end
+  end
+end
