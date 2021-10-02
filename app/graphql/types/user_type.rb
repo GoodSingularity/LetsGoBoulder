92e5a348bfe682f2707ffa0fc4838fa333fcf073
GoodSingularity <ZMosciszko@protonmail.com>
2021-10-02 13:51:00 -0400

    add points to user

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,7 +3,8 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :email, String, null: false
-
+    field :points, Int, null: false
+    
     def email
       object.email
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_702071) do
+ActiveRecord::Schema.define(version: 2021_10_02_702071) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_10_01_702071) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "archive", default: false
+    t.integer "points", default: 0
   end
 
 end
