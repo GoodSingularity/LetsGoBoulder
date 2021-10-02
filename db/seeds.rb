70554fbeba4d74db28a08b0cf5b6ac91c3d16c47
Kamil Mościszko <23016118+GoodSingularity@users.noreply.github.com>
2021-10-02 09:35:34 -0400

    [LSGB-055] Creating Route Seeds (#47)

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ User.create(name: Faker::Name.name, email: "admin@development.com", password_dig
 for i in 1..10 do
   User.create(name: Faker::Name.name, email: Faker::Internet.email, password_digest: "test", archive: false)
 end
+
+
+for i in 1..5 do
+  Route.create(name: Faker::Name.name, route_setter: User.first.name color: i, status: true, files: [])
+end
