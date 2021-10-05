cfc6a26d5b8b83acc47c1270abb0fef40c931809
Kamil Mościszko <23016118+GoodSingularity@users.noreply.github.com>
2021-10-05 15:25:09 -0400

    [LSGB-073] Adding RouteWasUpdated event (#76)

--- config/initializers/rails_event_store.rb
+++ config/initializers/rails_event_store.rb
@@ -2,4 +2,6 @@ Rails.configuration.to_prepare do
   $event_store = RailsEventStore::Client.new
   $event_store.subscribe(Context::Routes::Commands::Create.new, to: [RouteCreated])
   $event_store.subscribe(Context::Routes::Commands::DeleteFileFromRoute.new, to: [FileWasDeletedFromRoute])
+  $event_store.subscribe(Context::Routes::Commands::UpdateRoute.new, to: [RouteWasUpdated])
+
 end
