dcb6bfb61bd1d5e3c6e27f20c0ad5bc2ec607eb2
GoodSingularity <ZMosciszko@protonmail.com>
2021-10-14 08:47:04 -0400

    Adding makeUserHaveAdminPermissions mutation

---
+++ spec/graphql/mutations/make_user_have_admin_permissions_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+module Mutations
+  module Users
+    RSpec.describe MakeUserHaveAdminPermissionsMutation, type: :request do
+      let(:user) {
+        User.create!(
+          name: Faker::Name.name,
+          email: "test@test.com",
+          password: "test",
+          phone_number: 667089810,
+	  is_admin: true
+        )
+      }
+
+      let(:user_to_be_updated) {
+        User.create!(
+          name: Faker::Name.name,
+          email: "test123@test.com",
+          password: "test",
+          phone_number: 667089810,
+          is_admin: false
+        )
+      }
+
+      let(:not_valid_variables) do
+        {id: SecureRandom.uuid}
+      end
+
+      describe ".mutation passes" do
+        it "returns a true" do
+          result = FBoulderSchema.execute(query, variables: {id: user_to_be_updated.id}, context: {current_user: user})
+          user_to_be_updated.reload
+          expect(user_to_be_updated[:is_admin]).to eq true
+        end
+      end
+
+      def query
+        <<~GQL
+          mutation($id: ID!){
+            makeUserHaveAdminPermissions(input: {id: $id}){
+              clientMutationId
+              status
+            }
+          }
+        GQL
+      end
+    end
+  end
+end
