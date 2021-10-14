48aa3da2a7bf4eae0725f7d899e7b1008e0a3d4e
GoodSingularity <ZMosciszko@protonmail.com>
2021-10-14 10:19:30 -0400

    adding create route factories

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -4,15 +4,20 @@ require "faker"
 RSpec.describe Route, type: :model do
   context "when passes sucessful" do
     it "parrams ok" do
-      route = Route.create(name: Faker::Name.name, color: 1, route_setter: "Kacper")
+      route = create(:route, name: Faker::Name.name, color: rand(1..5), route_setter: Faker::Name.name)
       expect(route).to be_valid
     end
   end
 
   context "when does return failure" do
     it "color is not ok" do
-      route = Route.create(name: Faker::Name.name, color: "XD", route_setter: "Kacper")
-      expect(route).to_not be_valid
+      expect{
+	create(:route, 
+		name: Faker::Name.name, 
+		color: "XD", 
+		route_setter: Faker::Name.name
+	)
+	}.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 end
