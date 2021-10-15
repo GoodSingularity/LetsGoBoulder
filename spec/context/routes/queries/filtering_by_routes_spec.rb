37e2ffd45334d74124e6634dd2a40859e1a07f94
GoodSingularity <ZMosciszko@protonmail.com>
2021-10-15 07:01:25 -0400

    adding Queries specs of Context Routes

---
+++ spec/context/routes/queries/filtering_by_routes_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+module Resolvers
+  module Routes
+    RSpec.describe FilteringByRoutes, type: :request do
+      describe "Testing succesed in Query FilteringByRoutes in Routes Context" do
+        before do
+          create_list(:route, 10, color: 1)
+        end
+
+        before do
+          create_list(:route, 10, color: 2)
+        end
+
+        it "shows routes that has color 1 " do
+          args = { color: 1, route_setter: nil }
+          routes = Context::Routes::Queries::FilteringByRoutes.new.call(args: args)
+          expect(routes.size).to equal(10)
+        end
+      end
+
+      describe "Testing failed in Query FilteringByRoutes in Routes Context" do
+        it "does not show routes, because routes that has color 1 doest not exist " do
+          args = { color: 1, route_setter: nil }
+          expect { Context::Routes::Queries::FilteringByRoutes.new.call(args: args) }.to raise_error(Context::Routes::Errors::RouteNotFoundError)
+        end
+      end
+    end
+  end
+end
