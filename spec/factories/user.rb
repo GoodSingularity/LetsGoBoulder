743ada4b543c6f409075c1d46b651fcba1c128fd
GoodSingularity <ZMosciszko@protonmail.com>
2021-10-16 21:19:18 -0400

    test of details current user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     password_digest { Faker::Internet.password }
-    id { SecureRando.uuid }
+    id { SecureRandom.uuid }
     archive { false }
     points { 0 }
     avatar_id { nil }
