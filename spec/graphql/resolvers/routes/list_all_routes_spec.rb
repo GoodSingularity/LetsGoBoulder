f079f47f76d6948fd9c027396f1d88739911ed72
Kamil Mościszko <23016118+GoodSingularity@users.noreply.github.com>
2021-10-17 14:34:10 -0400

    [LSGB-093] Updating Routes Resolver Specs (#113)

--- spec/graphql/resolvers/routes/list_all_routes_spec.rb
+++ spec/graphql/resolvers/routes/list_all_routes_spec.rb
@@ -3,24 +3,43 @@ require "rails_helper"
 module Resolvers
   module Routes
     RSpec.describe ListAllRoutes, type: :request do
-      describe ".resolve" do
-        let(:user) {
-          User.create!(
-            name: "test",
-            email: "test@test.com",
-            password: "test",
-            phone_number: 667089810
-          )
+      let(:user) {
+        User.create!(
+          name: "test",
+          email: "test@test.com",
+          password: "test",
+          phone_number: 667089810
+        )
+      }
+      let(:context) {
+        ctx = {
+          current_user: user
         }
-        let(:context) {
-          ctx = {
-            current_user: user
-          }
-        }
+      }
+
+      describe "ListAllRoutes failed" do
         it "shows all routes" do
           post = FBoulderSchema.execute(query, variables: {}, context: context)
           error = post["errors"].first["message"]
           expect(error).eql?("Route is not found")
+        end
+      end
+
+      describe "ListAllRoutes succeed" do
+        before do
+          create(:route,
+                 name: Faker::Name.name,
+                 color: 1,
+                 route_setter: "test")
+          create(:route,
+                 name: Faker::Name.name,
+                 color: 1)
+        end
+
+        it "shows all routes" do
+          post = FBoulderSchema.execute(query: query, variables: {}, context: context)
+          size = post["data"]["listAllRoutes"].size
+          expect(size).equal?(2)
         end
       end
 
